cc6f5fb2252966fbf95fd4564eee56d7f16ad36d
Juan Schallmoser <schallmoserjuan@gmail.com>
2024-09-27 20:03:31 -0300

    Haciendo conexion desde un metodo

--- ProyectoFinal/DAL/conexionDAL.cs
+++ ProyectoFinal/DAL/conexionDAL.cs
@@ -5,21 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data; // Trabajar con SQL
 using MySql.Data.MySqlClient;
-using System.Data.SqlClient;
 
 namespace ProyectoFinal.DAL
 {
     internal class conexionDAL
     {
+        private string CadenaConexion = "server=localhost;port=3306;database=dbsistema;user=root;password=admin;";
+        MySqlConnection Conexion;
+
+        public MySqlConnection EstablecerConexion()
+        {
+             this.Conexion = new MySqlConnection(this.CadenaConexion);
+             return this.Conexion;
+        }
+
+
         //Creacion de metodo
         public bool PruebaConectar(){
             //Test para probar la conexion
             try {
-                MySqlConnection Conexion = new MySqlConnection("server=localhost;port=3306;database=dbsistema;user=root;password=admin;");
+                
                 MySqlCommand Comando = new MySqlCommand();
 
                 Comando.CommandText = "SELECT * FROM empleados";
-                Comando.Connection = Conexion;
+                Comando.Connection = this.EstablecerConexion();
                 Conexion.Open();
                 Comando.ExecuteNonQuery(); //
                 Conexion.Close();
